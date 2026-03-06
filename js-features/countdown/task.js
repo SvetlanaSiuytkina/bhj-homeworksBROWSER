a2f9dbedc60c0c19a034d9661d80c4820e479e5f
SVETA <svetadan1234@mail.ru>
2026-03-06 14:25:44 +0500

    first commit countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,57 @@
-let time = 60;
-let timer = document.getElementById("timer");
-let counter = setInterval(() => {
-    timer.innerHTML = "До окончания конкурса осталось секунд: ${time}";
-    time--;
+let time = 59;
+const timerElement = document.getElementById("timer");
 
-    if (timer < 0) {
+function updateTimer() {
+    timerElement.textContent = time;
+}
+
+const counter = setInterval(() => {
+    time--;
+    updateTimer();
+
+    if (time <= 0) {
         clearInterval(counter);
         alert ("Вы победили в конкурсе!");
     }
 }, 1000);
+
+
+// let time = "04:25:19";
+// const timerElement = document.getElementById("timer");
+
+// let timeSeconds = function parseTimeSeconds(time) {
+//     const [hours, minutes, seconds] = timeString.split(":").map(Namber);
+//     return hours * 3600 + minutes * 60 + seconds;
+// }
+
+// function conversionToSeconds(totalSeconds) {
+//     const hours = Math.trunc(totalSeconds / 3600);
+//     const minutes = Math.trunc(totalSeconds % 3600) / 60;
+//     const seconds = totalSeconds % 60;
+
+//     const addingLeadingZero = number => {
+//         if (number < 10) {
+//             return "0" + number;
+//         } else {
+//             return number;
+//         }
+//     }
+
+//     return `${addingLeadingZero(hours)}:${addingLeadingZero(minutes)}:${addingLeadingZero(seconds)}`
+// }
+
+// function updateTimer() {
+//     timerElement.textContent = conversionToSeconds(timeSeconds);
+// }
+// updateTimer();
+
+// const counter = setInterval(() => {
+//     if (timeSeconds > 0) {
+//         timeSeconds--;
+//         updateTimer();
+//     } else {
+//         clearInterval(counter);
+//         alert ("Вы победили в конкурсе!");
+//     }
+// }, 1000);
+
