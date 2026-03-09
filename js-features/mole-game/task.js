2ad8f26054aba9d1f2e965da0115f83fe7d5b073
SVETA <svetadan1234@mail.ru>
2026-03-09 22:53:04 +0500

    first commit mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,15 +2,45 @@ const deadElements = document.getElementById("dead");
 const lostElements = document.getElementById("lost");
 let killMoles = 0;
 let numberDefeats = 0;
-let victoryKillMoles = 10;
-let lossNumberDefeats = 5;
+const victoryKillMoles = 10;
+const lossNumberDefeats = 5;
 
-function getHole(index) {
-    return document.getElementById(`hole${index}`);
-}
+// function getHole(index) {
+//     return document.getElementById(`hole${index}`);
+// }
+
+const holes = document.querySelectorAll(".hole");
 
 function updateInterface() {
-    deadElements.textContent = dead;
-    lostElements.textContent = lost;
+    deadElements.textContent = killMoles;
+    lostElements.textContent = numberDefeats;
 }
 
+function gameStatus() {
+    if(killMoles === victoryKillMoles) {
+        alert("Победа!");
+        restartGame();
+    } else if (numberDefeats === lossNumberDefeats) {
+        alert("Поражение!");
+        restartGame();
+    }
+}
+
+function restartGame() {
+    killMoles = 0;
+    numberDefeats = 0;
+    updateInterface();
+}
+
+holes.forEach(hole => {
+    hole.addEventListener("click", function() {
+        if(this.classList.contains("hole_has-mole")) {
+            killMoles++;
+        } else {
+            numberDefeats++;
+        }
+        
+        updateInterface();
+        gameStatus()
+    });
+});
