4ce9c94559c65f0256f0d874a8ed1ee7772a1279
SVETA <svetadan1234@mail.ru>
2026-03-19 11:38:02 +0500

    исправлен

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,21 +1,16 @@
-const dropdown = document.querySelector(".dropdown");
-const dropdownValue = document.querySelector(".dropdown__value");
-const dropdownList = document.querySelector(".dropdown__list");
-const dropdownItems = document.querySelectorAll(".dropdown__item");
-
-dropdownValue.addEventListener("click", () => {
-    dropdownList.classList.toggle("dropdown__list_active");
-});
-
-function selectingMenuItem(event) {
+document.addEventListener("click", (event) => {
+    const dropdown = event.target.closest(".dropdown");
+    const dropdownValue = dropdown.querySelector(".dropdown__value");
+    const dropdownList = dropdown.querySelector(".dropdown__list");
     const link = event.target.closest(".dropdown__link");
+
+    if (event.target === dropdownValue) {
+        dropdownList.classList.toggle("dropdown__list_active");
+    }
+
     if (link) {
         event.preventDefault();
         dropdownValue.textContent = link.textContent;
         dropdownList.classList.remove("dropdown__list_active");
     }
-}
-
-dropdownItems.forEach(item => {
-    item.addEventListener("click", selectingMenuItem);
 });
