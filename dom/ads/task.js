f0a60c96b441c61875871f5c81c09d2e9e494aea
SVETA <svetadan1234@mail.ru>
2026-03-25 14:27:34 +0500

    done-ads

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function() {
     rotators.forEach(rotator => {
         const rotatorCases = rotator.querySelectorAll(".rotator__case");
         let currentIndex = 0;
-        let intervalId;
         
         function changingTextBlocks() {
             rotatorCases[currentIndex].classList.remove("rotator__case_active");
@@ -13,25 +12,13 @@ document.addEventListener("DOMContentLoaded", function() {
             rotatorCases[currentIndex].classList.add("rotator__case_active");
 
             const colorText = rotatorCases[currentIndex].getAttribute("data-color");
-            const speedTextChange = rotatorCases[currentIndex].getAttribute("data-speed");
+            const speedTextChange = Number(rotatorCases[currentIndex].getAttribute("data-speed"));
 
             rotator.style.color = colorText;
+
+            setTimeout(changingTextBlocks, speedTextChange);
         }
 
-        // function intervalDataSpeed(timeDelay) {
-        //     if(intervalId) {
-        //         clearInterval(intervalId);
-        //     }
-            
-        //     intervalId = setInterval(changingTextBlocks, timeDelay);
-        // }
-        
-        // const colorChange = rotatorCases[0].getAttribute("data-color");
-        // const speedChange = rotatorCases[0].getAttribute("data-speed");
-
-        // rotator.style.color = colorChange;
-        // intervalId = setInterval(changingTextBlocks, speedChange);
-        
-        setInterval(changingTextBlocks, 1000); // каждую секунду
+        setTimeout(changingTextBlocks, 1000);
     });
 });
