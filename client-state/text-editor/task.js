0442ebe8c02d019f8850618940987f7be342b73b
SVETA <svetadan1234@mail.ru>
2026-04-21 21:59:20 +0500

    first commit text-editor

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const editor = document.getElementById("editor");
+    const clearButton = document.getElementById("clearButton");
+
+    editor.addEventListener("input", () => {
+        const text = editor.value;
+        localStorage.setItem("textSaved", text);
+    });
+    
+    const textSaved = localStorage.getItem("textSaved");
+    if (textSaved) {
+        editor.value = textSaved
+    }
+    
+    clearButton.addEventListener("click", () => {
+        editor.value = "";
+        localStorage.removeItem("textSaved");
+    });
+});
