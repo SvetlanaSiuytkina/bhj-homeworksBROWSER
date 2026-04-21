797c5409e162f72eb9f511715acd3e85782e7389
SVETA <svetadan1234@mail.ru>
2026-04-21 16:13:10 +0500

    commit

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -12,25 +12,26 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         const formData = new FormData();
-        formData.append("file", fileInput.files);
+        formData.append("file", fileInput.files[0]);
 
         const xhr = new XMLHttpRequest();
-        xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
-        
-        xhr.addEventListener("progress", function(event) {
+
+        xhr.upload.addEventListener("progress", function(event) {
             if(event.total > 0) {
-                progress.value = (event.loaded / event.total);
+                const percentValue = (event.loaded / event.total) * 100;
+                progress.value = percentValue;
+                console.log(`Отправлено ${event.loaded} из ${event.total} (${percentValue.toFixed(2)}%)`);
             }
         });
-
-        xhr.addEventListener("load", function() {
-            if(xhr.status === 200) {
-                alert("Файл успешно загружен");
+        
+        xhr.onloadend = function() {
+            if (xhr.status === 200) {
+                console.log("Файл успешно загружен");
             } else {
-                alert("Ошибка при загрузке файла: " + xhr.status);
+                console.log("Ошибка при загрузке файла: " + xhr.status);
             }
-        });
-
+        }
+        xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
         xhr.send(formData);
     });
 });
