68258d988910f5f9f2f50f5d73ca601b75848038
SVETA <svetadan1234@mail.ru>
2026-04-25 23:05:33 +0500

    auth done

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,30 +1,36 @@
-const { use } = require("react");
-
 document.addEventListener("DOMContentLoaded", function() {
     const signinDiv = document.getElementById("signin");
     const signinForm = document.getElementById("signin__form");
     const signinBtn = document.getElementById("signin__btn");
     const welcomDiv = document.getElementById("welcome");
     const userIdElement = document.getElementById("user_id");
+    let errorElement = null;
 
     function sendAuthorizData(login, password) {
         const xhr = new XMLHttpRequest();
         xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/auth");
+        xhr.responseType = "json";
+        xhr.setRequestHeader("Content-Type", "application/json");
+
         xhr.onload = function() {
-            if (xhr.status === 200) {
-                try {
-                    const response = JSON.parse(xhr.responseText);
-                    successfulAuthoriz(response.userId);
-                } catch (error) {
-                    failedAuthoriz();
-                }
+            const response = xhr.response;
+            
+            if (response && response.userId) {
+                successfulAuthoriz(response.userId);
             } else {
                 failedAuthoriz();
             }
         }
-        xhr.send();
-    }
 
+        xhr.onerror = function() {
+            failedAuthoriz();
+        }
+
+        const authorizObject = {login, password};
+        const authorizData = JSON.stringify(authorizObject);
+        xhr.send(authorizData);
+    }
+    
     function successfulAuthoriz(userId) {
         localStorage.setItem("user_id", userId);
 
@@ -46,13 +52,11 @@ document.addEventListener("DOMContentLoaded", function() {
     function showError(message) {
         let errorElement = document.createElement("div");
         errorElement.classList.add("error-message");
-        signinForm.insertAdjacentElement(errorElement);
         errorElement.textContent = message;
         errorElement.style.display = "block";
     }
 
     function hideError() {
-        let errorElement = document.getElementById("error-message");
         if(errorElement) {
             errorElement.style.display = "none";
         }
@@ -60,12 +64,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     signinForm.addEventListener("submit", function(event) {
         event.preventDefault();
-        const login = document.querySelector("login");
-        const password = document.querySelector("password");
+
+        const login = document.querySelector("input[name='login']");
+        const password = document.querySelector("input[name='password']");
+
         sendAuthorizData(login, password);
     });
 
     const saveUserId = localStorage.getItem("user_id");
+
     if (saveUserId) {
         welcomDiv.classList.add("welcome_active");
         signinDiv.classList.remove("signin_active");
