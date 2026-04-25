9a92b5199b89c925aa169e57f60c2dddeaa95b28
SVETA <svetadan1234@mail.ru>
2026-04-25 22:21:00 +0500

    popup done

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -3,14 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const closeButton = document.querySelector(".modal__close");
 
     function setCookie(name, value) {
-        document.cookie = `${name} = encodeURIComponent(value); expires=Mon, 01 Jan 2018 00:00:00 GMT + "; " +
-        "path=/;"`
+        document.cookie = name + "=" + encodeURIComponent(value) + ";path=/;";
     }
 
     function getCookie(name) {
         const pairs = document.cookie.split("; ");
         const cookiePairs = pairs.find(p => p.startsWith(name + "="));
-        return cookiePairs.substr(name.length + 1);
+
+        if (!cookiePairs) 
+            return null;
+        return cookiePairs.slice(name.length + 1);
     }
 
     closeButton.addEventListener("click", () => {
